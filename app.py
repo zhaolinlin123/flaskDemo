2279e34e23472a81901cced115e9796b57432b97
Unknown <unknown@example.com>
2018-08-12 20:33:51 +0800

    菜单管理模块

--- app.py
+++ app.py
@@ -4,6 +4,12 @@ app = Flask(__name__)
 @app.route('/')
 def index():
 	return '<h1>Hello World!</h1>'
+
+
+@app.route('/menu')
+def menu():
+	return '<h1>menu</h1>'
+
 	
 if __name__ == '__main__':
 	app.run(debug=True)
